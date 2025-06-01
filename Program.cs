cffa93f8f63dfa29bb672e5a8f260da6e9ce9c5b
uditnagar2003 <uditnagar2003@gmail.com>
2025-06-01 16:10:14 +0530

    3.0

--- Program.cs
+++ Program.cs
@@ -1,15 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace VisualKeyloggerDetector
 {
     internal static class Program
     {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            VisualKeyloggerDetector.ApplicationConfiguration.Initialize();
-            Application.Run(new MainForm());
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            
         }
     }
 }
